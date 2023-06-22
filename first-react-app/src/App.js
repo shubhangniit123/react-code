c1946a36836a4b57a4fc495788bf96bdab0123e3
shugupta27 <shugupta27@publicisgroupe.net>
2023-06-22 14:45:09 +0530

    react code

---
+++ first-react-app/src/App.js
@@ -0,0 +1,58 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './App.css';
+/*function App() {
+  return (
+    <div className="App">   
+      <h1>Learn React on Tuesday</h1>       
+    </div>
+  );
+}
+
+export default App;*/
+/*
+// JSX Expression
+const vdate = new Date().toString();
+const element = 
+<div>
+<h1>Learn React on Day 2</h1>
+<h2>Today is : {vdate}</h2>
+</div>;
+ReactDOM.render(element, document.getElementById("root"));*/
+
+/*
+// working on object
+const user ={ empId : 123, empName : "Shubhang"};
+const element = <div>
+  <h1>User Data</h1>
+  <ul>
+    <li>
+      EmpId : {user.empId}
+    </li>
+    <li>
+      EmpName :{user.empName}
+    </li>
+  </ul>
+</div>
+ReactDOM.render(element, document.getElementById("root"));*/
+/*
+// Working on JSX function
+function getDateTime(){
+  return new Date().toString();
+}
+const element = <h1>Current Date Time : {getDateTime()}</h1>
+ReactDOM.render(element, document.getElementById("root"));*/
+
+/*
+// Working with if statment
+const x =1;
+let data ="Bye";
+if(x<10){
+ data ="Hello"; 
+}
+
+const element = <h1>{data}</h1>
+ReactDOM.render(element, document.getElementById("root"));*/
+//export default App;
---
+++ first-react-app/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+//import reportWebVitals from './reportWebVitals';
+
+//ReactDOM.render(<App />, document.getElementById('root'));
+/*function demo(){
+  const element =(<div>
+    <h1>Working with React Js</h1>
+    <h2> {new Date().toLocaleTimeString()}</h2>
+  
+    </div>  
+  )
+ReactDOM.render(element, document.getElementById('root'));
+}
+setInterval(demo, 1000);*/
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+//reportWebVitals();
